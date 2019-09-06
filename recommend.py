51fcd258d7c80824efe67f4c2a13b18d07426832
Lesliewinnie <52366163+Lesliewinnie@users.noreply.github.com>
2019-09-06 11:47:46 +0800

    Update recommend.py

--- recommend.py
+++ recommend.py
@@ -10,36 +10,69 @@ from deepctr.inputs import SparseFeat, VarLenSparseFeat,get_fixlen_feature_names
 from bert_serving.client import BertClient
 bc=BertClient()
 
+def recommend_resume2job(job_id):#输入职位id，得到推荐的简历id
+    for i in range(0,len(job)):
+        if(job.iloc[i,0]==job_id):
+            print("vccccccccc")
+            temp=job.iloc[i]
+            temp.values[-4]=bc.encode([str(temp.values[-4])]).tolist()
+            temp.values[-1]=bc.encode([str(temp.values[-1])]).tolist()
+            break
+    result=[]
+    for i in range(0,len(resume)):
+        if((len(resume.loc[i,['workingExperiences']].values[0])>=temp.values[4])&
+           (resume.loc[i,['basicInfo']].values[0]['location'] in str(temp.values[-3]))):
+            test_input=[np.array([len(resume.loc[i,['workingExperiences']].values[0])]),
+                        pd.Series(temp.values[-4]),pd.Series(temp.values[-1]),pd.Series([resume.loc[i,['skill']].values[0]])]
+            result.append([resume.loc[i,['userId']].values[0],model.predict(test_input, batch_size=256)])
+    result.sort(key=lambda x:x[1],reverse=True)
+    code=[]
+    length=min(10,len(result))
+    for i in range(length):
+        code.append(result[i][0])
+    res={job_id:code}
+    return res
+def recommend_job2resume(resume_id):#输入简历id，得到推荐的职位id
+    for i in range(0,len(resume)):
+        if(str(resume.iloc[i,0])==resume_id):
+            temp=resume.iloc[i]
+            break                
+    result=[]
+    for i in range(0,len(job)):
+        if((len(temp['workingExperiences'])>=job.iloc[i,4])
+           &(temp['basicInfo']['location'] in job.loc[i,'work_place'])):
+            test_input=[np.array([len(temp['workingExperiences'])]),
+                        pd.Series(job.iloc[i,-4]),pd.Series(job.iloc[i,-1]),pd.Series([temp['skill']])]
+            result.append([job.loc[i,'id'],model.predict(test_input, batch_size=256)])
+    result.sort(key=lambda x:x[1],reverse=True)
+    code=[]
+    length=min(10,len(result))
+    for i in range(length):
+        code.append(result[i][0])
+    res={resume_id:code}
+    return res
+
+def combine(x):
+    print(x)
+    skill=""
+    for i in range(len(x)):
+        skill=skill+x[i]['skill']
+    return skill
+
+#训练模型
 training_data = pd.read_excel("C:/Users/admin/Desktop/1.xlsx")#已标注的训练数据
-resume= pd.read_excel("C:/Users/admin/Desktop/resume_new.xlsx")#简历数据库
-#job= pd.read_excel("C:/Users/admin/Desktop/job_new1.xlsx")
+resume= load_data.load_resume_from_service()#简历数据库
 job=load_data.load_job_from_mysql()#职位数据库
+resume['skill']=list(map(combine,resume['skills']))
+for i in range(len(resume)):
+    if(resume.loc[i,['basicInfo']].values[0]['location']!=None):
+        resume.loc[i,['basicInfo']].values[0]['location']=resume.loc[i,['basicInfo']].values[0]['location'].replace("/","-")
+    else:
+        resume.loc[i,['basicInfo']].values[0]['location']=""
 job.columns=['id','company_id','release_time','job_posting_line','work_exp','edu_exp','job_nature','min_salary','max_salary','position_labels','job_description','work_place','online_state','job_name']
-#data1 = pd.read_excel("C:/Users/admin/Documents/2.xlsx")
 all_column=list(range(len(training_data.columns.values)))
-#max1=max(data["prof_skill_id"].values)
-max2=max(training_data["project_exp_id"].values)
-max3=max(training_data["school_exp_id"].values)
-max4=max(training_data["paper_id"].values)
-max5=max(training_data["working_exp_id"].values)
-sparse_features = ["project_exp_id","school_exp_id","paper_id","working_exp_id"]
-#dense_features=["job_description","job_name","self_description","expected_job","hobbies"]
+sparse_features = ["working_exp_id"]
 dense_features=["job_description","job_name","skill"]
-#for i in range(0,max1):
-   # dense_features.append("skill"+str(i))
-'''for i in range(0,max2):
-    dense_features.append("associated_company"+str(i))
-    dense_features.append("project_content"+str(i))
-    dense_features.append("project_name"+str(i))
-for i in range(0,max3):
-    dense_features.append("school_exp_name"+str(i))
-for i in range(0,max4):
-    dense_features.append("paper_name"+str(i))
-for i in range(0,max5):
-    dense_features.append("company_name"+str(i))
-    dense_features.append("department"+str(i))
-    dense_features.append("job_content"+str(i))
-    dense_features.append("job_name"+str(i))'''
 
 target = ['label']
 # 1.Label Encoding for sparse features,and process sequence features
@@ -51,11 +84,11 @@ for i in dense_features:
     training_data[i]=bc.encode(training_data[i].values.tolist()).tolist()
 for i in ['job_description','job_name']:
     job[i]=bc.encode(job[i].tolist()).tolist()
-for i in ["skill"]:
-    resume[i]=bc.encode(resume[i].values.tolist()).tolist()
-   # print(i)
-#for i in dense_features:
- #   data[i]=list(map(embedding,list(data[i].values)))
+for i in range(len(resume)):
+    if(resume.loc[i,'skill']!=''):
+        resume.loc[i,'skill']=bc.encode([resume.loc[i,'skill']])
+    else:
+        resume.loc[i,'skill']=[0]*768  
 fixlen_feature_columns = [SparseFeat(feat, training_data[feat].nunique())
                     for feat in sparse_features]
 varlen_feature_columns=[]
@@ -63,9 +96,7 @@ varlen_input = []
 for feat in dense_features:
     varlen_input=varlen_input+[training_data[feat]]
 # 2.count #unique features for each sparse field and generate feature config for sequence feature
-#varlen_feature_columns2 = [VarLenSparseFeat(feat, len(data[feat].values)+1,max(np.array(list(map(len, data[feat].values)))))
-                    #for feat in dense_features]  # Notice : value 0 is for padding for sequence input feature
-    
+
 
 linear_feature_columns = fixlen_feature_columns + varlen_feature_columns
 dnn_feature_columns = fixlen_feature_columns + varlen_feature_columns
@@ -81,45 +112,8 @@ model = DeepFM(linear_feature_columns,dnn_feature_columns,task='regression')
 model.compile("adam", "mse", metrics=['mse'], )
 history = model.fit(model_input, training_data[target].values,
                     batch_size=256, epochs=10, verbose=2, validation_split=0.2, )
+#示例
+result=recommend_job2resume("f66959f9-e64c-408c-8f82-55058c777640")
+print(result)
 
-choose=input("Recommend resumes for jobs: input 0,else input 1:")
-if(choose=='0'):#输入职位id，得到推荐的简历id
-    job_id=input("Please input the job id:")
-    for i in range(0,len(job)):
-        if(job.iloc[i,0]==job_id):
-            temp=job.iloc[i]
-            temp.values[-4]=bc.encode([str(temp.values[-4])]).tolist()
-            temp.values[-1]=bc.encode([str(temp.values[-1])]).tolist()
-            break
-    result=[]
-    for i in range(0,len(resume)):
-        if((resume.loc[i,['working_exp_id']].values[0]>=temp.values[4])&(resume.loc[i,['school_exp_id']].values[0]>=temp.values[5])
-           &(resume.loc[i,['work_place']].values[0] in str(temp.values[-3]))):
-            test_input=[np.array([resume.loc[i,['project_exp_id']].values[0]]),np.array([resume.loc[i,['school_exp_id']].values[0]]),
-                        np.array([resume.loc[i,['paper_id']].values[0]]),np.array([resume.loc[i,['working_exp_id']].values[0]]),
-                        pd.Series(temp.values[-4]),pd.Series(temp.values[-1]),pd.Series([resume.loc[i,['skill']].values[0]])]
-            result.append([resume.loc[i,['id']].values[0],model.predict(test_input, batch_size=256)])
-    result.sort(key=lambda x:x[1],reverse=True)
-    code=[]
-    for i in range(0,len(result)):
-        code.append(result[i][0])
-    print(code)
-elif(choose=='1'):#输入简历id，得到推荐的职位id
-    resume_id=input("Please input the resume id:")
-    for i in range(0,len(resume)):
-        if(str(resume.iloc[i,0])==resume_id):
-            temp=resume.iloc[i]
-            break
-    result=[]
-    for i in range(0,len(job)):
-        if((temp['working_exp_id']>=job.iloc[i,4])&(temp['school_exp_id']>=job.iloc[i,5])
-           &(temp['work_place'] in job.loc[i,'work_place'])):
-            test_input=[np.array([temp['project_exp_id']]),np.array([temp['school_exp_id']]),
-                        np.array([temp['paper_id']]),np.array([temp['working_exp_id']]),
-                        pd.Series(job.iloc[i,-4]),pd.Series(job.iloc[i,-1]),pd.Series([temp['skill']])]
-            result.append([job.loc[i,'id'],model.predict(test_input, batch_size=256)])
-    result.sort(key=lambda x:x[1],reverse=True)
-    code=[]
-    for i in range(0,len(result)):
-        code.append(result[i][0])
-    print(code)
+
